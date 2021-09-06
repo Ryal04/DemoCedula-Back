4d2f8010b7ed8c901a14956db80395507f3ffcdc
COMFENALCO\practecnologia03 <roidy.1207@gmail.com>
2021-09-06 09:09:41 -0500

    RE1-CDEV5

---
+++ DemoTextract-Back/Controllers/TextController.cs
@@ -0,0 +1,71 @@
+﻿using Amazon.Textract;
+using Amazon.Textract.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoTextract_Back.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TextController : ControllerBase
+    {
+        private readonly IAmazonTextract amazonTextract;
+
+        public TextController(IAmazonTextract amazonTextract)
+        {
+            this.amazonTextract = amazonTextract;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Text([FromForm] IFormFile CedDorsal)
+        {
+            var cont = 0;
+            var list = new List<String>();
+            Document ImageTarget = new Document();
+
+            //Convercion Imagen a Bytes
+            using (var ms = new MemoryStream())
+            {
+                CedDorsal.CopyTo(ms);
+                ImageTarget.Bytes = new MemoryStream(ms.ToArray());
+            }
+
+            var response = await amazonTextract.DetectDocumentTextAsync(new DetectDocumentTextRequest()
+            {
+                Document = ImageTarget,
+            });
+
+            foreach (var data in response.Blocks)
+            {
+                
+                if (data.BlockType.Value == "LINE" && data.Confidence >= 85)
+                {
+                    cont++;
+
+                    if (data.Text != "FECHA DE NACIMIENTO" && cont == 1)
+                    {
+                            list.Add(data.Text); 
+                    }
+
+                    if (data.Text != "LUGAR DE NACIMIENTO" 
+                        && data.Text != "SEXO" 
+                        && data.Text != "G.S. RH" 
+                        && data.Text != "ESTATURA" 
+                        && cont > 1 && cont < 13) {
+
+                        list.Add(data.Text);
+                    }
+
+                }
+            }
+
+            return Ok(list);
+        }
+    }
+}
+
