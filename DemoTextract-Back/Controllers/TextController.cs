e17258802f0611f7ba2adbc3f24e9d04aad2d73e
COMFENALCO\practecnologia03 <roidy.1207@gmail.com>
2021-09-06 09:16:36 -0500

    CV1-CDEV1

--- DemoTextract-Back/Controllers/TextController.cs
+++ DemoTextract-Back/Controllers/TextController.cs
@@ -42,26 +42,45 @@ namespace DemoTextract_Back.Controllers
 
             foreach (var data in response.Blocks)
             {
-                
+
                 if (data.BlockType.Value == "LINE" && data.Confidence >= 85)
                 {
                     cont++;
 
                     if (data.Text != "FECHA DE NACIMIENTO" && cont == 1)
                     {
-                            list.Add(data.Text); 
+                        list.Add(data.Text);
                     }
 
-                    if (data.Text != "LUGAR DE NACIMIENTO" 
-                        && data.Text != "SEXO" 
-                        && data.Text != "G.S. RH" 
-                        && data.Text != "ESTATURA" 
-                        && cont > 1 && cont < 13) {
-
+                    if (data.Text != "LUGAR DE NACIMIENTO"
+                    && data.Text != "SEXO"
+                    && data.Text != "G.S. RH"
+                    && data.Text != "ESTATURA"
+                    && cont > 1 && cont < 13)
+                    {
                         list.Add(data.Text);
                     }
 
                 }
+            }
+
+            string[] fechanacimiento = list[0].Split(' ');
+            if (fechanacimiento.Length > 1)
+            {
+                list[0] = fechanacimiento[3];
+            }
+
+            string[] DepartamentoNacimiento = list[2].Split('(', ')');
+            if (DepartamentoNacimiento.Length > 1)
+            {
+                list[2] = DepartamentoNacimiento[1];
+            }
+
+            string[] fechaylugarexp = list[6].Split(' ');
+            if (fechaylugarexp.Length > 1)
+            {
+                list[6] = fechaylugarexp[1];
+                list[7] = fechaylugarexp[0];
             }
 
             return Ok(list);
